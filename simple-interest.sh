96370e1249e91f63011dd0c48bbb9fae78bc9f8f
DevOps_Culture_in_SDLC <90929082+vncharyhub@users.noreply.github.com>
2025-04-27 11:38:51 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,19 @@
-echo "simple-interest.sh file is presented"
+#!/bin/bash
+
+# simple-interest.sh
+# Formula: Simple Interest = (Principal * Rate * Time) / 100
+
+echo "Enter Principal Amount:"
+read principal
+
+echo "Enter Rate of Interest:"
+read rate
+
+echo "Enter Time (in years):"
+read time
+
+# Calculate Simple Interest
+simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
+echo "Simple Interest is: ₹$simple_interest"
+
